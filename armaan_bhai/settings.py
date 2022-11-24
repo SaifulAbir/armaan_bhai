ae4246cb6f2975628c8d0708d70ce5fb6ae2b972
Saiful Islam <saiful.abir20@gmail.com>
2022-11-24 16:46:18 +0600

    api documentation added

--- armaan_bhai/settings.py
+++ armaan_bhai/settings.py
@@ -44,8 +44,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    'drf_yasg',
 
-    'user'
+    'user',
 ]
 
 MIDDLEWARE = [
--- armaan_bhai/urls.py
+++ armaan_bhai/urls.py
@@ -15,8 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API for Armaan Bhai",
+      default_version='v1',
+      # description="Test description",
+      # terms_of_service="https://www.google.com/policies/terms/",
+      # contact=openapi.Contact(email="contact@snippets.local"),
+      # license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('user.urls')),
+    path('v1/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]

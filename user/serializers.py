55f91fc5126b6dd119b0d450d29ceda55ad54cf4
Saiful Islam <saiful.abir20@gmail.com>
2023-02-14 17:05:17 +0600

    user profile api is modified

--- user/serializers.py
+++ user/serializers.py
@@ -250,7 +250,7 @@ class UserProfileDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'full_name', 'gender', 'organization_name', 'address', 'division', 'district', 'upazilla',
-                  'village', 'postcode', 'phone_number', 'image', 'gender_display_value', 'user_type']
+                  'village', 'postcode', 'phone_number', 'image', 'gender_display_value', 'user_type', 'is_superuser']
 
 
 class ProductSerializer(serializers.ModelSerializer):
